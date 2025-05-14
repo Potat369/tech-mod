855ef1aefd89b8c351c9cc93936e22abed2e23f1
Potat369 <yevheniidemian@gmail.com>
2025-05-14 11:26:54 +0300

    Initial commit

---
+++ src/client/java/techmod/TechModDataGenerator.java
@@ -0,0 +1,11 @@
+package techmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TechModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}

3bef9d242fe0b47547f8f6285bff5a5a6cae40d2
Nanokerb <1102david2007@gmail.com>
2025-05-15 09:04:43 +0300

    Add drill heads

--- src/main/java/techmod/registry/ModItems.java
+++ src/main/java/techmod/registry/ModItems.java
@@ -10,6 +10,13 @@ import java.util.function.Function;
 
 public class ModItems {
 
+    public static final Item COPPER_DRILL_HEAD = registerItem("copper_drill_head", Item::new, new Item.Settings());
+    public static final Item IRON_DRILL_HEAD = registerItem("iron_drill_head", Item::new, new Item.Settings());
+    public static final Item GOLD_DRILL_HEAD = registerItem("gold_drill_head", Item::new, new Item.Settings());
+    public static final Item DIAMOND_DRILL_HEAD = registerItem("diamond_drill_head", Item::new, new Item.Settings());
+    public static final Item EMERALD_DRILL_HEAD = registerItem("emerald_drill_head", Item::new, new Item.Settings());
+    public static final Item NETHERITE_DRILL_HEAD = registerItem("netherite_drill_head", Item::new, new Item.Settings());
+
     public static Item registerItem(String name, Function<Item.Settings, Item> factory, Item.Settings settings) {
         final RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, TechMod.idOf(name));
         return Items.register(itemKey, factory, settings);

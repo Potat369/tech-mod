5df438174c6e4621e122c68244c69bf7276ac4a1
Potat369 <yevheniidemian@gmail.com>
2025-05-16 13:04:32 +0300

    Make drill heads use components

--- src/main/java/techmod/registry/ModItems.java
+++ src/main/java/techmod/registry/ModItems.java
@@ -1,27 +1,46 @@
 package techmod.registry;
 
+import net.minecraft.block.Block;
+import net.minecraft.component.type.ToolComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryEntryLookup;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.BlockTags;
 import techmod.TechMod;
 import techmod.item.DrillItem;
+import techmod.item.ModToolMaterials;
 
+import java.util.List;
 import java.util.function.Function;
 
 public class ModItems {
 
-    public static final Item COPPER_DRILL_HEAD = registerItem("copper_drill_head", Item::new, new Item.Settings());
-    public static final Item IRON_DRILL_HEAD = registerItem("iron_drill_head", Item::new, new Item.Settings());
-    public static final Item GOLD_DRILL_HEAD = registerItem("gold_drill_head", Item::new, new Item.Settings());
-    public static final Item DIAMOND_DRILL_HEAD = registerItem("diamond_drill_head", Item::new, new Item.Settings());
-    public static final Item EMERALD_DRILL_HEAD = registerItem("emerald_drill_head", Item::new, new Item.Settings());
-    public static final Item NETHERITE_DRILL_HEAD = registerItem("netherite_drill_head", Item::new, new Item.Settings());
+    public static final Item COPPER_DRILL_HEAD = registerDrillHead("copper", ModToolMaterials.COPPER);
+    public static final Item IRON_DRILL_HEAD = registerDrillHead("iron", ToolMaterial.IRON);
+    public static final Item GOLD_DRILL_HEAD = registerDrillHead("gold", ToolMaterial.GOLD);
+    public static final Item DIAMOND_DRILL_HEAD = registerDrillHead("diamond", ToolMaterial.DIAMOND);
+    public static final Item EMERALD_DRILL_HEAD = registerDrillHead("emerald", ModToolMaterials.EMERALD);
+    public static final Item NETHERITE_DRILL_HEAD = registerDrillHead("netherite", ToolMaterial.NETHERITE);
     public static final Item DRILL = registerItem("drill", DrillItem::new, new Item.Settings());
 
     private static Item registerItem(String name, Function<Item.Settings, Item> factory, Item.Settings settings) {
         final RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, TechMod.idOf(name));
         return Items.register(itemKey, factory, settings);
+    }
+
+    private static Item registerDrillHead(String material, ToolMaterial toolMaterial) {
+        RegistryEntryLookup<Block> registryEntryLookup = Registries.createEntryLookup(Registries.BLOCK);
+        return registerItem(material + "_drill_head", Item::new, new Item.Settings()
+                .repairable(toolMaterial.repairItems())
+                .maxDamage(toolMaterial.durability())
+                .component(ModComponents.RULES, List.of(
+                        ToolComponent.Rule.ofNeverDropping(registryEntryLookup.getOrThrow(toolMaterial.incorrectBlocksForDrops())),
+                        ToolComponent.Rule.ofAlwaysDropping(registryEntryLookup.getOrThrow(BlockTags.PICKAXE_MINEABLE), toolMaterial.speed()))
+                ));
     }
 
     public static void registerItems() {

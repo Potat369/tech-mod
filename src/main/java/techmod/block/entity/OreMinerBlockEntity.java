674ffd6d9f92b556e7e9f0d9fa4e2672cbfe3783
Potat369 <yevheniidemian@gmail.com>
2025-05-20 09:39:48 +0300

    Remove content in tick method

--- src/main/java/techmod/block/entity/OreMinerBlockEntity.java
+++ src/main/java/techmod/block/entity/OreMinerBlockEntity.java
@@ -4,17 +4,13 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-
-import techmod.TechMod;
 import techmod.registry.ModBlockEntities;
 
 public class OreMinerBlockEntity extends BlockEntity {
+
     public OreMinerBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntities.ORE_MINER, pos, state);
     }
 
-    public static void tick(World world, BlockPos blockPos, BlockState blockState, OreMinerBlockEntity entity) {
-        world.breakBlock(blockPos.up(), false);
-        TechMod.LOGGER.info(Boolean.toString(world.isClient()));
-    }
+    public static void tick(World world, BlockPos blockPos, BlockState blockState, OreMinerBlockEntity entity) {}
 }

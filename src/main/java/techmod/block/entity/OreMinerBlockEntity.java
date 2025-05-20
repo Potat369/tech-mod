ba18c8a142a7d9fbb160f2aef59b7c7ad1244fe3
Potat369 <yevheniidemian@gmail.com>
2025-05-20 10:50:20 +0300

    Add mining logic to ore miner

--- src/main/java/techmod/block/entity/OreMinerBlockEntity.java
+++ src/main/java/techmod/block/entity/OreMinerBlockEntity.java
@@ -5,6 +5,10 @@ import net.fabricmc.fabric.api.tag.convention.v2.ConventionalBlockTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.HopperBlockEntity;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -14,12 +18,59 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class OreMinerBlockEntity extends BlockEntity {
+    private HashMap<Block, HashSet<BlockPos>> ores;
+    private static final int SPEED = 5;
+    private int timer;
 
     public OreMinerBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntities.ORE_MINER, pos, state);
     }
 
-    public static void tick(World world, BlockPos blockPos, BlockState blockState, OreMinerBlockEntity entity) {}
+    public static void tick(World world, BlockPos blockPos, BlockState blockState, OreMinerBlockEntity entity) {
+        if (!world.isClient()) {
+            if (entity.timer++ >= OreMinerBlockEntity.SPEED) {
+                entity.timer = 0;
+                if (entity.ores == null) {
+                    entity.ores = getOresInChunk(world, blockPos);
+                }
+                if (!entity.ores.isEmpty()) {
+                    for (var block : entity.ores.keySet()) {
+                        for (var pos : entity.ores.get(block)) {
+                            entity.ores.get(block).remove(pos);
+                            if (world.getBlockState(pos).getBlock() == block && world.breakBlock(pos, false)) {
+                                var inventory = HopperBlockEntity.getInventoryAt(world, blockPos.up());
+                                if (inventory != null) {
+                                    var insertableSlot = getInsertableSlot(inventory, block.asItem().getDefaultStack());
+                                    if (insertableSlot == -1) {
+                                        world.spawnEntity(new ItemEntity(world, blockPos.toCenterPos().x, blockPos.toCenterPos().y + 1, blockPos.toCenterPos().z, block.asItem().getDefaultStack()));
+                                    }
+                                    else {
+                                        var newStack = inventory.getStack(insertableSlot);
+                                        newStack.increment(1);
+                                        inventory.setStack(insertableSlot, newStack);
+                                    }
+                                }
+                                else {
+                                    world.spawnEntity(new ItemEntity(world, blockPos.toCenterPos().x, blockPos.toCenterPos().y + 1, blockPos.toCenterPos().z, block.asItem().getDefaultStack()));
+                                }
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static int getInsertableSlot(Inventory inventory, ItemStack stack) {
+        for (var i = 0; i < inventory.size(); i++) {
+            var slotStack = inventory.getStack(i);
+            if (slotStack.getCount() < slotStack.getMaxCount() && (slotStack.isOf(stack.getItem()) || slotStack.isEmpty())) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
     public static HashMap<Block, HashSet<BlockPos>> getOresInChunk(World world, BlockPos blockPos) {
         var ores =

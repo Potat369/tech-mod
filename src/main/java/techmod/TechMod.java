5a466a8a4db78d56cefcffa0f5a781637659c12b
Nanokerb <1102david2007@gmail.com>
2025-05-28 17:21:22 +0300

    Add golden modules to dungeon loot

--- src/main/java/techmod/TechMod.java
+++ src/main/java/techmod/TechMod.java
@@ -1,15 +1,19 @@
 package techmod;
 
 import net.fabricmc.api.ModInitializer;
-
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import techmod.registry.*;
+import techmod.util.ModLootTablesModifier;
 
 public class TechMod implements ModInitializer {
     public static final String MOD_ID = "tech-mod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public static Identifier idOf(String path) {
+        return Identifier.of(MOD_ID, path);
+    }
 
     @Override
     public void onInitialize() {
@@ -20,9 +24,6 @@ public class TechMod implements ModInitializer {
         ModParticleTypes.init();
         ModBlocks.init();
         ModBlockEntities.init();
-    }
-
-    public static Identifier idOf(String path) {
-        return Identifier.of(MOD_ID, path);
+        ModLootTablesModifier.init();
     }
 }
---
+++ src/main/java/techmod/util/ModLootTablesModifier.java
@@ -0,0 +1,35 @@
+package techmod.util;
+
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTables;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import techmod.registry.ModItems;
+
+public class ModLootTablesModifier {
+
+    public static void init() {
+        LootTableEvents.MODIFY.register(((key, tableBuilder, source, registries) -> {
+            if (key.equals(LootTables.ABANDONED_MINESHAFT_CHEST)) {
+                LootPool.Builder builder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.05f).build())
+                        .with(ItemEntry.builder(ModItems.MODULE_EFFICIENCY_5))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1, 1)));
+                tableBuilder.pool(builder.build());
+            }
+            if (key.equals(LootTables.BASTION_TREASURE_CHEST)) {
+                LootPool.Builder builder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.05f).build())
+                        .with(ItemEntry.builder(ModItems.MODULE_FORTUNE_3))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1, 1)));
+                tableBuilder.pool(builder.build());
+            }
+        }));
+    }
+}

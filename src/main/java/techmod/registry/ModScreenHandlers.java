19836303edcbfee7764b774e8e1b826556200c3f
Potat369 <yevheniidemian@gmail.com>
2025-11-28 12:23:28 +0200

    Format ModScreenHandlers

--- src/main/java/techmod/registry/ModScreenHandlers.java
+++ src/main/java/techmod/registry/ModScreenHandlers.java
@@ -8,12 +8,12 @@ import techmod.TechMod;
 import techmod.screen.DrillScreenHandler;
 
 public class ModScreenHandlers {
-    private static <T extends ScreenHandlerType<?>> T register(String id, T type) {
-        return Registry.register(Registries.SCREEN_HANDLER, TechMod.idOf(id), type);
-    }    public static final ScreenHandlerType<DrillScreenHandler> DRILL_SCREEN_HANDLER =
+    public static final ScreenHandlerType<DrillScreenHandler> DRILL_SCREEN_HANDLER =
             register("drill", new ScreenHandlerType<>(DrillScreenHandler::new, FeatureFlags.VANILLA_FEATURES));
 
+    private static <T extends ScreenHandlerType<?>> T register(String id, T type) {
+        return Registry.register(Registries.SCREEN_HANDLER, TechMod.idOf(id), type);
+    }
+
     public static void init() {}
-
-
 }

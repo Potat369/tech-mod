db6ce76a54d418b96a7d03b98563fe38ff6a0770
Potat369 <yevheniidemian@gmail.com>
2025-12-03 16:54:11 +0200

    Implement fortune module

---
+++ src/main/java/techmod/mixin/ApplyBonusLootFunctionMixin.java
@@ -0,0 +1,27 @@
+package techmod.mixin;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.item.ItemStack;
+import net.minecraft.loot.function.ApplyBonusLootFunction;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import techmod.item.DrillItem;
+import techmod.registry.ModComponents;
+import techmod.registry.ModTags;
+
+@Mixin(ApplyBonusLootFunction.class)
+public class ApplyBonusLootFunctionMixin {
+    @ModifyVariable(method = "process", at = @At("STORE"), ordinal = 0)
+    int b(int value, @Local(ordinal = 1) ItemStack stack) {
+        if (value == 0 && stack.getItem() instanceof DrillItem) {
+            var module = DrillItem.getModules(stack)
+                    .filter(s -> s.isIn(ModTags.FORTUNE_MODULES))
+                    .findFirst();
+            if (module.isPresent()) {
+                return module.get().get(ModComponents.MODULE).level();
+            }
+        }
+        return value;
+    }
+}

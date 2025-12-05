f128c9201aa614a2360ce2df644e40efcd417cf5
Potat369 <yevheniidemian@gmail.com>
2025-12-05 14:29:17 +0200

    Properly format ModBlockEntities

--- src/main/java/techmod/registry/ModBlockEntities.java
+++ src/main/java/techmod/registry/ModBlockEntities.java
@@ -12,21 +12,20 @@ import techmod.block.entity.OreMinerBlockEntity;
 import techmod.block.entity.TeslaCoilBlockEntity;
 
 public class ModBlockEntities {
+    public static final BlockEntityType<OreMinerBlockEntity> ORE_MINER =
+            of("ore_miner", OreMinerBlockEntity::new, ModBlocks.ORE_MINER);
+    public static final BlockEntityType<MelterBlockEntity> MELTER =
+            of("melter", MelterBlockEntity::new, ModBlocks.MELTER);
+    public static final BlockEntityType<TeslaCoilBlockEntity> TESLA_COIL =
+            of("tesla_coil", TeslaCoilBlockEntity::new, ModBlocks.TESLA_COIL);
+
+    public static void init() {}
+
     private static <T extends BlockEntity> BlockEntityType<T> of(
             String name, FabricBlockEntityTypeBuilder.Factory<? extends T> entityFactory, Block... blocks) {
         return Registry.register(
                 Registries.BLOCK_ENTITY_TYPE,
                 TechMod.idOf(name),
                 FabricBlockEntityTypeBuilder.<T>create(entityFactory, blocks).build());
-    }    public static final BlockEntityType<OreMinerBlockEntity> ORE_MINER =
-            of("ore_miner", OreMinerBlockEntity::new, ModBlocks.ORE_MINER);
-
-    public static void init() {}    public static final BlockEntityType<MelterBlockEntity> MELTER =
-            of("melter", MelterBlockEntity::new, ModBlocks.MELTER);
-    public static final BlockEntityType<TeslaCoilBlockEntity> TESLA_COIL =
-            of("tesla_coil", TeslaCoilBlockEntity::new, ModBlocks.TESLA_COIL);
-
-
-
-
+    }
 }

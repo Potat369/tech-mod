77f8cbb88453f1af00809126527342c9be775938
Potat369 <yevheniidemian@gmail.com>
2025-12-08 13:24:48 +0200

    Format ItemTagProvider

--- src/client/java/techmod/datagen/ItemTagProvider.java
+++ src/client/java/techmod/datagen/ItemTagProvider.java
@@ -19,39 +19,43 @@ public class ItemTagProvider extends FabricTagProvider<Item> {
     @Override
     protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
         getOrCreateTagBuilder(ModItemTags.COPPER_TOOL_MATERIALS).add(Items.COPPER_INGOT);
+
         getOrCreateTagBuilder(ModItemTags.EMERALD_TOOL_MATERIALS).add(Items.EMERALD);
+
         getOrCreateTagBuilder(ModItemTags.DRILL_HEADS)
-                .add(
-                        ModItems.COPPER_DRILL_HEAD,
-                        ModItems.IRON_DRILL_HEAD,
-                        ModItems.GOLDEN_DRILL_HEAD,
-                        ModItems.DIAMOND_DRILL_HEAD,
-                        ModItems.EMERALD_DRILL_HEAD,
-                        ModItems.NETHERITE_DRILL_HEAD);
+                .add(ModItems.COPPER_DRILL_HEAD)
+                .add(ModItems.IRON_DRILL_HEAD)
+                .add(ModItems.GOLDEN_DRILL_HEAD)
+                .add(ModItems.DIAMOND_DRILL_HEAD)
+                .add(ModItems.EMERALD_DRILL_HEAD)
+                .add(ModItems.NETHERITE_DRILL_HEAD);
+
         getOrCreateTagBuilder(ModItemTags.MODULES)
-                .add(
-                        ModItems.MODULE_DEPTH,
-                        ModItems.MODULE_WIDTH,
-                        ModItems.MODULE_HEIGHT,
-                        ModItems.MODULE_EFFICIENCY_1,
-                        ModItems.MODULE_EFFICIENCY_2,
-                        ModItems.MODULE_EFFICIENCY_3,
-                        ModItems.MODULE_EFFICIENCY_4,
-                        ModItems.MODULE_EFFICIENCY_5,
-                        ModItems.MODULE_FORTUNE_1,
-                        ModItems.MODULE_FORTUNE_2,
-                        ModItems.MODULE_FORTUNE_3,
-                        ModItems.MODULE_ENERGY_EFFICIENCY,
-                        ModItems.MODULE_MAGNETISM,
-                        ModItems.MODULE_SILK_TOUCH);
+                .add(ModItems.MODULE_DEPTH)
+                .add(ModItems.MODULE_WIDTH)
+                .add(ModItems.MODULE_HEIGHT)
+                .add(ModItems.MODULE_EFFICIENCY_1)
+                .add(ModItems.MODULE_EFFICIENCY_2)
+                .add(ModItems.MODULE_EFFICIENCY_3)
+                .add(ModItems.MODULE_EFFICIENCY_4)
+                .add(ModItems.MODULE_EFFICIENCY_5)
+                .add(ModItems.MODULE_FORTUNE_1)
+                .add(ModItems.MODULE_FORTUNE_2)
+                .add(ModItems.MODULE_FORTUNE_3)
+                .add(ModItems.MODULE_ENERGY_EFFICIENCY)
+                .add(ModItems.MODULE_MAGNETISM)
+                .add(ModItems.MODULE_SILK_TOUCH);
+
         getOrCreateTagBuilder(ModItemTags.EFFICIENCY_MODULES)
-                .add(
-                        ModItems.MODULE_EFFICIENCY_1,
-                        ModItems.MODULE_EFFICIENCY_2,
-                        ModItems.MODULE_EFFICIENCY_3,
-                        ModItems.MODULE_EFFICIENCY_4,
-                        ModItems.MODULE_EFFICIENCY_5);
+                .add(ModItems.MODULE_EFFICIENCY_1)
+                .add(ModItems.MODULE_EFFICIENCY_2)
+                .add(ModItems.MODULE_EFFICIENCY_3)
+                .add(ModItems.MODULE_EFFICIENCY_4)
+                .add(ModItems.MODULE_EFFICIENCY_5);
+
         getOrCreateTagBuilder(ModItemTags.FORTUNE_MODULES)
-                .add(ModItems.MODULE_FORTUNE_1, ModItems.MODULE_FORTUNE_2, ModItems.MODULE_FORTUNE_3);
+                .add(ModItems.MODULE_FORTUNE_1)
+                .add(ModItems.MODULE_FORTUNE_2)
+                .add(ModItems.MODULE_FORTUNE_3);
     }
 }

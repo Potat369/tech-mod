791ac2ae3f8681117d39547613e1b4ad99419655
Potat369 <yevheniidemian@gmail.com>
2025-12-15 22:32:45 +0200

    Add FluidTankWidget

---
+++ src/client/java/techmod/gui/widget/FluidTankWidget.java
@@ -0,0 +1,96 @@
+package techmod.gui.widget;
+
+import com.mojang.blaze3d.vertex.VertexFormat;
+import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
+import net.minecraft.client.gui.widget.ClickableWidget;
+import net.minecraft.client.render.BuiltBuffer;
+import net.minecraft.client.render.RenderLayers;
+import net.minecraft.client.render.Tessellator;
+import net.minecraft.client.render.VertexFormats;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+
+import java.util.function.Supplier;
+
+public class FluidTankWidget extends ClickableWidget {
+    private final Fluid fluid;
+    private final FluidState state;
+    private final Supplier<Integer> fluidAmountGetter;
+    private final Supplier<Integer> fluidCapacityGetter;
+    private final MinecraftClient client;
+
+    public FluidTankWidget(
+            int x,
+            int y,
+            int width,
+            int height,
+            Fluid fluid,
+            FluidState state,
+            Supplier<Integer> fluidAmountGetter,
+            Supplier<Integer> fluidCapacityGetter) {
+        super(x, y, width, height, Text.literal("Fluid Tank"));
+        this.fluid = fluid;
+        this.state = state;
+        this.fluidAmountGetter = fluidAmountGetter;
+        this.fluidCapacityGetter = fluidCapacityGetter;
+        this.client = MinecraftClient.getInstance();
+    }
+
+    @Override
+    protected void renderWidget(DrawContext context, int mouseX, int mouseY, float deltaTicks) {
+        var sprite = FluidRenderHandlerRegistry.INSTANCE.get(fluid)
+                .getFluidSprites(this.client.world, BlockPos.ORIGIN, state)[0];
+        var consumer = Tessellator.getInstance()
+                .begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR_TEXTURE_LIGHT_NORMAL);
+        var capacity = fluidCapacityGetter.get();
+        var amount = fluidAmountGetter.get();
+        var fluidHeight = (double) height * ((double) amount / capacity);
+        for (int i = 0; i < Math.ceil((double) width / 16); i++) {
+            for (int j = 0; j < Math.ceil(fluidHeight) / 16; j++) {
+                int xOffset = getX() + 16 * i;
+                int yOffset = getY() + height - 16 * j;
+                consumer.vertex(xOffset, yOffset, 15)
+                        .texture(sprite.getMinU(), sprite.getMaxV())
+                        .normal(0, 0, 0)
+                        .color(0xFFFFFFFF)
+                        .light(15728880);
+                consumer.vertex(xOffset + 16, yOffset, 15)
+                        .texture(sprite.getMaxU(), sprite.getMaxV())
+                        .normal(0, 0, 0)
+                        .color(0xFFFFFFFF)
+                        .light(15728880);
+                consumer.vertex(xOffset + 16, yOffset - 16, 15)
+                        .texture(sprite.getMaxU(), sprite.getMinV())
+                        .normal(0, 0, 0)
+                        .color(0xFFFFFFFF)
+                        .light(15728880);
+                consumer.vertex(xOffset, yOffset - 16, 15)
+                        .texture(sprite.getMinU(), sprite.getMinV())
+                        .normal(0, 0, 0)
+                        .color(0xFFFFFFFF)
+                        .light(15728880);
+            }
+        }
+        BuiltBuffer builtBuffer = consumer.endNullable();
+        if (builtBuffer != null) {
+            context.enableScissor(getX(), (int) (getY() + height - fluidHeight), getX() + width, getY() + height);
+            RenderLayers.getFluidLayer(state).draw(builtBuffer);
+            context.disableScissor();
+        }
+        if (isHovered()) {
+            context.drawTooltip(
+                    client.textRenderer,
+                    Text.literal(String.format("%,dmb / %,dmb", amount, capacity)),
+                    mouseX,
+                    mouseY);
+        }
+    }
+
+    @Override
+    protected void appendClickableNarrations(NarrationMessageBuilder builder) {}
+}
--- src/client/java/techmod/screen/MelterScreen.java
+++ src/client/java/techmod/screen/MelterScreen.java
@@ -2,25 +2,21 @@ package techmod.screen;
 
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
-import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.*;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.MathHelper;
 import techmod.TechMod;
 import techmod.block.entity.MelterBlockEntity;
-
-import java.util.function.Function;
+import techmod.gui.widget.FluidTankWidget;
 
 public class MelterScreen extends HandledScreen<MelterScreenHandler> {
-    private static final Identifier LAVA_TEXTURE = Identifier.ofVanilla("textures/block/lava_still.png");
     private static final Identifier TEXTURE = TechMod.idOf("textures/gui/melter_v2.png");
     private static final Identifier PROGRESS_ARROW = TechMod.idOf("textures/gui/arrow_progress.png");
     private static final int TANK_HEIGHT = 61;
     private static final int TANK_WIDTH = 21;
-    private static int lastLavaAmount = 0;
-    private static int initialLavaAmount = 0;
-    private static float animationProgress = 0f;
+    private static FluidTankWidget fluidTankWidget;
 
     public MelterScreen(MelterScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
@@ -28,50 +24,27 @@ public class MelterScreen extends HandledScreen<MelterScreenHandler> {
     }
 
     @Override
+    protected void init() {
+        super.init();
+        fluidTankWidget = new FluidTankWidget(x + 139, y + 17, TANK_WIDTH, TANK_HEIGHT, Fluids.LAVA, Fluids.LAVA.getStill(false), handler::getLavaAmount, () -> MelterBlockEntity.maxLava);
+        addDrawableChild(fluidTankWidget);
+    }
+
+    @Override
     protected void drawBackground(DrawContext context, float deltaTicks, int mouseX, int mouseY) {
-        int i = (this.width - backgroundWidth) / 2;
-        int j = (this.height - backgroundHeight) / 2;
         context.drawTexture(
-                RenderLayer::getGuiTextured, TEXTURE, i, j, 0.0F, 0.0F, backgroundWidth, backgroundHeight, 256, 256);
+                RenderLayer::getGuiTextured, TEXTURE, x, y, 0.0F, 0.0F, backgroundWidth, backgroundHeight, 256, 256);
         renderProgressArrow(context, x, y);
     }
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float deltaTicks) {
         super.render(context, mouseX, mouseY, deltaTicks);
-        int i = (this.width - backgroundWidth) / 2;
-        int j = (this.height - backgroundHeight) / 2;
-        int lavaAmount = handler.getLavaAmount();
-        if (lavaAmount != lastLavaAmount) {
-            animationProgress = 0;
-            initialLavaAmount = lastLavaAmount;
-            lastLavaAmount = lavaAmount;
-        }
-        float lastFullness = (float) initialLavaAmount / MelterBlockEntity.maxLava;
-        float newFullness = (float) handler.getLavaAmount() / MelterBlockEntity.maxLava;
-        int height = MathHelper.lerp(
-                (float) (Math.sin((animationProgress * Math.PI) / 2)),
-                (int) (TANK_HEIGHT * (1.0f - lastFullness)),
-                (int) (TANK_HEIGHT * (1.0f - newFullness)));
-        context.drawTexture(
-                RenderLayer::getGuiTextured,
-                LAVA_TEXTURE,
-                i + 139,
-                j + 17 + height,
-                0.0f,
-                0.0f,
-                TANK_WIDTH,
-                TANK_HEIGHT - height,
-                16,
-                320);
-        if (animationProgress < 1.0f) {
-            animationProgress = Math.clamp(animationProgress + 0.05f * deltaTicks, 0.0f, 1.0f);
-        }
         drawMouseoverTooltip(context, mouseX, mouseY);
     }
 
     private void renderProgressArrow(DrawContext context, int x, int y) {
-        if(handler.isCrafting()){
+        if (handler.isCrafting()) {
             int progress = handler.getScaledArrowProgress();
             int fullHeight = 25;
             int width = 18;
@@ -80,9 +53,16 @@ public class MelterScreen extends HandledScreen<MelterScreenHandler> {
             int arrowY = y + 34;
 
             context.drawTexture(
-                    RenderLayer::getGuiTextured,PROGRESS_ARROW, arrowX, arrowY, 0,0, width, progress, width, fullHeight
-            );
-
+                    RenderLayer::getGuiTextured,
+                    PROGRESS_ARROW,
+                    arrowX,
+                    arrowY,
+                    0,
+                    0,
+                    width,
+                    progress,
+                    width,
+                    fullHeight);
         }
     }
 }
